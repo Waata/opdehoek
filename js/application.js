28d948c0cce5eec82559aab0043fb57cf1efd5b6
WouterKompier <wouterjckompier@gmail.com>
2016-02-20 14:46:22 +0100

    lay-out update

---
+++ js/application.js
@@ -0,0 +1,9 @@
+// $( document ).ready(function() {
+//   // Handler for .ready() called.
+
+// $( "#" ).click(function() {
+//   $( "#navbar-header" ).hide( "slow", function() {
+//     alert( "WELKOM BIJ OPDEHOEK" );
+//   });
+// });
+//   
