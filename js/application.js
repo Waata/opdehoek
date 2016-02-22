e6c038bd9e89c6785e60f73d285ce20a0af75aae
WouterKompier <wouterjckompier@gmail.com>
2016-02-22 15:01:00 +0100

    navbar animation

--- js/application.js
+++ js/application.js
@@ -1,6 +1,39 @@
-// $(document).ready(function(){
-//   // Set the interval to be 5 seconds
-//   var t = setInterval(function(){
-//     $("#carousel ol").animate({marginLeft:-480},1000)
-//   },2000);
-// });
+(function ($) {
+var isNavBarShown = true;
+
+    $(document).ready(function () {
+
+        // hide .navbar first
+        // $(".navbar").hide(false);
+
+        // fade in .navbar
+            $(window).scroll(function () {
+                // set distance user needs to scroll before we fadeIn navbar
+                if ($(this).scrollTop() > 450) {
+                    $('.navbar').fadeOut();
+                    transitionTime: 150,
+                    isNavBarShown = true;
+
+                } else {
+                $('.navbar').fadeIn();
+                isNavBarShown = false;
+            } 
+        });
+        /* Show navbar */
+
+        $('#shownav').hover(function () {
+            if (isNavBarShown) { return; }
+            $('.navbar').fadeIn();
+        });
+
+        /* when navbar is hovered over it will override previous */
+
+        $('.navbar').hover(function () {
+            if (isNavBarShown) { return; }
+            $('.navbar').show();
+        }, function () {
+            if (isNavBarShown) { return; }
+            $('.navbar').fadeOut();
+        });
+    });
+}(jQuery));
