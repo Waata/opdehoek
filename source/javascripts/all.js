1ae0187a55c03cab5dcabab908c306dd978268a9
WouterKompier <wouterjckompier@gmail.com>
2016-02-28 21:54:46 +0100

    middleman first

---
+++ source/javascripts/all.js
@@ -0,0 +1,52 @@
+// = require jquery
+// = require bootstrap-sprockets
+// = require_tree .
+
+
+
+// alert("Welkom op de Beta site van OpDeHoek")
+
+(function ($) {
+var isNavBarShown = true;
+
+    $(document).ready(function () {
+
+        // hide .navbar first
+        // $(".navbar").hide(false);
+
+        // fade in .navbar
+            $(window).scroll(function () {
+                // set distance user needs to scroll before we fadeIn navbar
+                if ($(this).scrollTop() > 200) {
+                    $('.navbar').fadeOut();
+                    transitionTime: 2200    ,
+                    isNavBarShown = true;
+
+                } else {
+                $('.navbar').fadeIn();
+                isNavBarShown = false;
+
+            } 
+        });
+        /* Show navbar */
+
+        $('#shownav').hover(function () {
+            if (isNavBarShown) { return; }
+            $('.navbar').fadeIn();
+        });
+
+        /* when navbar is hovered over it will override previous */
+
+        $('.navbar').hover(function () {
+            if (isNavBarShown) { return; }
+            $('.navbar').show();
+        }, function () {
+            if (isNavBarShown) { return; }
+            $('.navbar').fadeOut();
+        });
+    });
+}(jQuery));
+
+
+
+
