4883cce4bd2d7a95104c661b39898015cbaa6ab8
WouterKompier <wouterjckompier@gmail.com>
2016-04-14 22:31:51 +0200

    minor changes

---
+++ source/javascripts/navbar.js
@@ -0,0 +1,37 @@
+(function ($) {
+var isNavBarShown = true;
+
+    $(document).ready(function () {
+
+        // fade in .navbar
+            $(window).scroll(function () {
+                // set distance user needs to scroll before we fadeIn navbar
+                if ($(this).scrollTop() > 200) {
+                    $('.navbar').fadeOut();
+                    transitionTime: 2200    ,
+                    isNavBarShown = true;
+
+                } else {
+                $('.navbar').fadeIn();
+                isNavBarShown = false;
+
+            } 
+        });
+        /* Show navbar */
+
+        $('#shownav').hover(function () {
+            if (isNavBarShown) { return; }
+            $('.navbar').fadeIn();
+        });
+
+        /* when navbar is hovered over it will override previous */
+
+        $('.navbar').hover(function () {
+            if (isNavBarShown) { return; }
+            $('.navbar').show();
+        }, function () {
+            if (isNavBarShown) { return; }
+            $('.navbar').fadeOut();
+        });
+    });
+}(jQuery));
